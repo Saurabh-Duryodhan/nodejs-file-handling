922694658db5932257456d557c4752229ef854c3
Saurabh R. Duryodhan <srduryodhan97@gmail.com>
2024-06-10 22:22:41 +0530

    Initial Commit

---
+++ src/server.js
@@ -0,0 +1,14 @@
+dotenv.config()
+import express from 'express'
+import dotenv from 'dotenv'
+import { join, resolve } from 'path';
+
+const __dirname = resolve()
+
+const { PORT } = process.env
+const server = express()
+
+server.use(express.static(join(__dirname, "public/uploads")))
+
+server.get('/templates', (req, res) => res.sendFile(join(__dirname, "public/uploads", 'wallpaper.jpg')))
+server.listen(PORT, () => console.log(`Server started at port number: http://localhost:${PORT}`))
